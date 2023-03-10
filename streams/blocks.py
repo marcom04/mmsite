4dd7324b4b4e873cb3e5ab9b663f6bbc38b1ef61
Marco Marchini <marco.marchini@cyberoo51.com>
2023-03-10 16:48:35 +0100

    Add StreamBlocks.

--- streams/blocks.py
+++ streams/blocks.py
@@ -1,4 +1,5 @@
 from wagtail.core import blocks
+from wagtail.images import blocks as images_blocks
 
 
 class TitleAndTextBlock(blocks.StructBlock):
@@ -11,3 +12,50 @@ class TitleAndTextBlock(blocks.StructBlock):
         template = "streams/title_and_text_block.html"
         icon = "edit"
         label = "Title & Text"
+
+
+class RichTextBlock(blocks.RichTextBlock):
+    """Rich text with all the features."""
+
+    class Meta:
+        template = "streams/richtext_block.html"
+        icon = "doc-full"
+        label = "Full RichText"
+
+
+class SimpleRichTextBlock(blocks.RichTextBlock):
+    """Rich text with limited features."""
+
+    def __init__(self, required=True, help_text=None, editor='default', features=None, **kwargs):
+        super().__init__()
+        self.features = [
+            'bold',
+            'italic',
+            'link',
+        ]
+
+    class Meta:
+        template = "streams/richtext_block.html"
+        icon = "edit"
+        label = "Simple RichText"
+
+
+class CardBlock(blocks.StructBlock):
+    """Cards with image and text and button(s)."""
+    title = blocks.CharBlock(required=True, help_text="Add your title")
+    cards = blocks.ListBlock(
+        blocks.StructBlock(
+            [
+                ("image", images_blocks.ImageChooserBlock(required=True)),
+                ("title", blocks.CharBlock(required=True, max_length=40)),
+                ("text", blocks.TextBlock(reqruired=True, max_length=200)),
+                ("button_page", blocks.PageChooserBlock(required=False)),
+                ("button_url", blocks.URLBlock(required=False, help_text="If the button page above is selected, that will be used first.")),
+            ]
+        )
+    )
+
+    class Meta:
+        template = "streams/card_block.html"
+        icon = "placeholder"
+        label = "Staff Cards"
